69e6bdd957b7fbb49610b3c44d26e354e350183b
Nick Gerakines <ngerakines@blizzard.com>
2013-03-01 11:28:41 -0800

    updated distclean rule to not remove config.h.cmake files

--- cmake/TargetDistclean.cmake
+++ cmake/TargetDistclean.cmake
@@ -3,7 +3,7 @@ IF (UNIX)
   ADD_CUSTOM_COMMAND(
     COMMENT "distribution clean"
     COMMAND $(MAKE_COMMAND) clean &&
-      find . \\! -path "./cmake/\\*" -path "\\*.cmake" -o -name CMakeFiles -o -name Makefile -o -name CMakeCache.txt -o -name Testing -o -name cmake_install.cmake -o -name install_manifest.txt -o -name "*.so" | xargs rm -rf
+      find . \\! -path "./cmake/\\*" \\! -name config.h.cmake -path "\\*.cmake" -o -name CMakeFiles -o -name Makefile -o -name CMakeCache.txt -o -name Testing -o -name cmake_install.cmake -o -name install_manifest.txt -o -name "*.so" | xargs rm -rf
     TARGET distclean
   )
 ENDIF(UNIX)
